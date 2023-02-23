f2cd095e1ac0a4571721c4c066155aeeaa8174cc
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-02-23 16:35:17 +0100

    iteration 4 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import IdCard from './components/IdCard';
 import Greetings from './components/Greetings';
 import Random from './components/Random';
 import berlinData from './data/berlin.json';
+import BoxColor from './components/BoxColor';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <IdCard firstName={firstName} lastName={lastName} gender="fluid" picture={picture} birth={new Date()} height="1.70" />
       <Greetings lang='fr '>{firstName}</Greetings>
       <Random max="3" min="1" />
+      <BoxColor r="250" g="100" b="200" />
     </div>
   );
 }
---
+++ src/components/BoxColor.js
@@ -0,0 +1,13 @@
+export default function BoxColor(props) {
+    const r = parseInt(props.r)
+    const g = parseInt(props.g)
+    const b = parseInt(props.b)
+    const hexIt = color => color.toString(16).padStart(2, '0').toUpperCase()
+    const hex = `#${hexIt(r)}${hexIt(g)}${hexIt(b)}`
+    console.log(hex);
+
+    return <div className="card" style={{backgroundColor: hex}}>
+        <p>rgb({r},{g},{b})</p>
+        <p>{hex}</p>
+    </div>
+}
