6d5c6208a1310f6107edf596c4ba276e15625f60
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2023-02-25 17:08:08 +0100

    iteration 10 implemented

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rating from './components/Rating';
 import DriverCard from './components/DriverCard';
 import LikeButton from './components/LikeButton';
 import ClickablePicture from './components/ClickablePicture';
+import Dice from './components/Dice';
 
 import './App.css';
 
@@ -97,6 +98,8 @@ function App() {
       <LikeButton />
 
       <ClickablePicture img="maxence.png" imgClicked="maxence-glasses.png" />
+
+      <Dice />
     </div>
   );
 }
---
+++ src/components/Dice.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import diceEmpty from '../assets/images/dice-empty.png'
+import dice1 from '../assets/images/dice1.png'
+import dice2 from '../assets/images/dice2.png'
+import dice3 from '../assets/images/dice3.png'
+import dice4 from '../assets/images/dice4.png'
+import dice5 from '../assets/images/dice5.png'
+import dice6 from '../assets/images/dice6.png'
+
+export default function Dice () {
+    const diceArray = [dice1, dice2, dice3, dice4, dice5, dice6]
+
+    const [dice, setDice] = useState(diceEmpty)
+
+    const clickHandler = () => { 
+        setDice(diceEmpty)
+        setTimeout(()=>{
+            const newDice = Math.floor(Math.random()*6)
+            setDice(diceArray[newDice])
+        }, 1000)
+     }
+
+    return (
+        <img src={dice} alt="dice!" onClick={clickHandler} style={{height: '400px'}}/>
+    )
+}
